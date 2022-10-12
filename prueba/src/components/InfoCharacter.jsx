522ce4d842895d5108f2631b982d8a5ad5ba3e53
mTorrendell <mercedestorrendell@gmail.com>
2022-10-12 17:56:42 -0300

    styles to infoCharacter

--- prueba/src/components/InfoCharacter.jsx
+++ prueba/src/components/InfoCharacter.jsx
@@ -6,6 +6,9 @@ import Backbone from "backbone";
 
 function InfoCharacter({ characterId }) {
   const [character, setCharacter] = useState("");
+  const [showOrigin, setShowOrigin] = useState(false);
+  const [showLocation, setShowLocation] = useState(false);
+  const [showEpisodes, setShowEpisodes] = useState(false);
 
   useEffect(() => {
     const model = new Backbone.Model();
@@ -27,7 +30,7 @@ function InfoCharacter({ characterId }) {
     );
   }
 
-  return (
+  return character ? (
     <>
       <img className="backGroundImg" src={backGroundGif}></img>
       <div className="infoIndividual">
@@ -43,7 +46,7 @@ function InfoCharacter({ characterId }) {
         </section>{" "}
         <section className="d-flex justify-content-around align-items-center">
           {" "}
-          <h1 className="nameCharacter"> {character.name}</h1>
+          <h1 className="nameCharacter"> {character.name.toUpperCase()}</h1>
           <img
             className="imageIndividualCharacter d-sm-inline d-none"
             src={character.image}
@@ -52,41 +55,87 @@ function InfoCharacter({ characterId }) {
         <section>
           <section className="d-flex justify-content-between">
             {" "}
-            <div></div>
-            <div></div>
+            <div className="styleButtons"></div>
+            <div className="styleButtons"></div>
           </section>
-          <section className="mt-4 infoIndividual d-flex justify-content-between">
+          <section className="mt-4 infoIndividual d-md-flex d-block	justify-content-around">
             <section>
-              <h2>{character.Id}</h2>
-              <h2>
+              <h2 className="infoCharacter">CHARACTER: {character.id}</h2>
+              <h2 className="infoCharacter">
+                STATUS:{" "}
                 {character.status ? character.status : "No recorded status"}
               </h2>
-              <h2>
+              <h2 className="infoCharacter">
+                SPECIE:{" "}
                 {character.species ? character.species : "No recorded species"}
               </h2>
-              <h2>{character.type ? character.type : "No recorded type"}</h2>
-              <h2>
+              <h2 className="infoCharacter">
+                TYPE: {character.type ? character.type : "No recorded type"}
+              </h2>
+              <h2 className="infoCharacter">
+                GENDER{" "}
                 {character.gender ? character.gender : "No recorded gender"}
               </h2>
             </section>
             <section>
-              <div className="mt-4">
-                <h2>Origin</h2>
-                <p className="infoButton">HOLA HOLA</p>
-              </div>
-              <div className="mt-4">
-                <h2>Location</h2>
-                <p className="infoButton">HOLA HOLA</p>
-              </div>
-              <div className="mt-4">
-                <h2>Episodes</h2>
-                <p className="infoButton">HOLA HOLA</p>
-              </div>
+              <section className="mt-4">
+                <h2
+                  className="infoCharacterButton"
+                  onClick={() => {
+                    showOrigin ? setShowOrigin(false) : setShowOrigin(true);
+                  }}
+                >
+                  Origin
+                </h2>
+                {showOrigin && (
+                  <section className="infoCharacter">
+                    <p className="infoButton">Name: {character.origin.name} </p>
+                  </section>
+                )}
+              </section>
+              <section className="mt-4">
+                <h2
+                  className="infoCharacterButton"
+                  onClick={() => {
+                    showLocation
+                      ? setShowLocation(false)
+                      : setShowLocation(true);
+                  }}
+                >
+                  Location
+                </h2>
+                {showLocation && (
+                  <section className="infoCharacter">
+                    <p className="infoButton">
+                      Name: {character.location.name}
+                    </p>
+                  </section>
+                )}
+              </section>
+              <section className="mt-4">
+                <h2
+                  className="infoCharacterButton"
+                  onClick={() => {
+                    showEpisodes
+                      ? setShowEpisodes(false)
+                      : setShowEpisodes(true);
+                  }}
+                >
+                  Episodes
+                </h2>
+                {showEpisodes && (
+                  <section className="infoCharacter">
+                    <p className="infoButton">H</p>
+                  </section>
+                )}
+              </section>
             </section>
           </section>
         </section>
       </div>
     </>
+  ) : (
+    <div>Loading...</div>
   );
 }
 
